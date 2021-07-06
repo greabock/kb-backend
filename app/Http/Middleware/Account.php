0181f84615fbdd4da5470a3fbba73712a25753c2
Роман Сохарев <greabock@gmail.com>
2021-07-06 20:08:02 +0700

    Добавил заготовку Middleware для управления аккаунтом

--- app/Http/Middleware/Account.php
+++ app/Http/Middleware/Account.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+
 class Account
 {
-
+    public function handle(Request $request, Closure $next)
+    {
+        // TODO: check account available
+        // TODO: select account database
+        return $next($request);
+    }
 }
