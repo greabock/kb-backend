420fca0c89d3278da7ec8064b53f356795fefb6e
Роман Сохарев <greabock@gmail.com>
2021-07-07 14:01:07 +0700

    Переместил Action разделов  внутрь API

---
+++ app/Http/Actions/API/Sections/Index/Action.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Actions\Api\Sections\Index;
+
+class Action
+{
+    public function __invoke()
+    {
+        return response()->json([]);
+    }
+}
