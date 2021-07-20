ecb6297738d8b57f4ca5d13ab7dd171dcfd45dee
Роман Сохарев <greabock@gmail.com>
2021-07-20 19:29:47 +0700

    Добавил тест на отображение материала

--- app/Http/Actions/Api/Materials/Show/Action.php
+++ app/Http/Actions/Api/Materials/Show/Action.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Actions\Api\Materials\Show;
 
+use App\Http\Actions\Api\Materials\Update\Request;
+use App\Http\Resources\MaterialResource;
+use App\Models\Section;
+use Illuminate\Http\JsonResponse;
+
 class Action
 {
-    public function __invoke()
+    public function __invoke(Section $section, Request $request): JsonResponse
     {
+        $material = ($section->class_name)::findOrFail($request->material);
 
+        return (new MaterialResource($material))->response()->setStatusCode(200);
     }
 }
--- tests/Feature/Api/Materials/ShowTest.php
+++ tests/Feature/Api/Materials/ShowTest.php
@@ -4,7 +4,44 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\Materials;
 
-class ShowTest
-{
+use App\Models\Material;
+use App\Models\Section;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Api\ActionTestCase;
 
+class ShowTest extends ActionTestCase
+{
+    use WithFaker;
+
+    public function getRouteName(): string
+    {
+        return 'sections.materials.show';
+    }
+
+
+    public function testUserCanSeeMaterialDetails(): void
+    {
+        /** @var Section $section */
+        $section = Section::factory()->has(
+            Section\Field::factory()->count(4),
+            'fields',
+        )->create();
+
+        $section->refresh();
+
+        $fieldValues = $section->fields->keyBy('id')->map(fn() => $this->faker->word)
+            ->toArray();
+
+        /** @var Material $material */
+        $material = $this->populator()->populate(
+            $section->class_name,
+            array_merge(['name' => 'Name'], $fieldValues)
+        );
+
+        $this->populator()->flush();
+
+        $this->callAuthorizedRouteAction([], ['material' => $material->id, 'section' => $section->id])
+            ->assertJsonStructure(['data' => array_keys($fieldValues)])
+            ->assertOk();
+    }
 }
