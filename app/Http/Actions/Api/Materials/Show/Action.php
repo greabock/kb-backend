2406c235005172b653bc400950b60216b6e35e21
Роман Сохарев <greabock@gmail.com>
2021-08-19 17:14:41 +0700

    Возвращать материал с отношениями

--- app/Http/Actions/Api/Materials/Show/Action.php
+++ app/Http/Actions/Api/Materials/Show/Action.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Actions\Api\Materials\Show;
 
-use App\Http\Actions\Api\Materials\Update\Request;
-use App\Http\Resources\MaterialResource;
 use App\Models\Section;
+use App\Models\Material;
+use App\Http\Resources\MaterialResource;
+use App\Http\Actions\Api\Materials\Update\Request;
 use Illuminate\Http\JsonResponse;
 
 class Action
 {
     public function __invoke(Section $section, Request $request): JsonResponse
     {
-        $material = ($section->class_name)::findOrFail($request->material);
+        /** @var Material $material */
+        $material = ($section->class_name)::with(
+            $section->getRelationFields()->pluck('id')->toArray()
+        )->findOrFail($request->material);
 
         return (new MaterialResource($material))->response()->setStatusCode(200);
     }
--- tests/Feature/Api/Materials/ShowTest.php
+++ tests/Feature/Api/Materials/ShowTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\Materials;
 
+use App\Models\File;
 use App\Models\Material;
 use App\Models\Section;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithFaker;
+use Ramsey\Uuid\Uuid;
 use Tests\Feature\Api\ActionTestCase;
 
 class ShowTest extends ActionTestCase
@@ -42,5 +45,54 @@ class ShowTest extends ActionTestCase
         $this->callAuthorizedRouteAction([], ['material' => $material->id, 'section' => $section->id])
             ->assertJsonStructure(['data' => array_keys($fieldValues)])
             ->assertOk();
+    }
+
+    public function testMaterialReturnWithRelations(): void
+    {
+        /** @var Section $section */
+        $section = Section::factory()->has(
+            Section\Field::factory(['type' => ['name' => 'List', 'of' => ['name' => 'File']]]),
+            'fields',
+        )->create();
+
+        $section->refresh();
+
+        $file = File::create([
+            'id' => $fileId = Uuid::uuid4()->toString(),
+            'realpath' => '_',
+            'url' => route('files.download', [$fileId]),
+            'indexed' => true,
+            'content' => <<<TEXT
+            Laravel невероятно масштабируем.
+            Благодаря удобному для масштабирования характеру PHP и встроенной
+            поддержке быстрых распределенных систем кеширования, таких как Redis,
+            горизонтальное масштабирование с Laravel очень просто. Фактически, приложения
+            Laravel легко масштабируются для обработки сотен миллионов запросов в месяц.
+            Требуется экстремальное масштабирование? Такие платформы, как Laravel Vapor, позволяют запускать
+             приложение Laravel в практически неограниченном масштабе
+            с использованием новейшей бессерверной технологии AWS.
+            TEXT,
+            'name' => 'Трактат о Laravel',
+            'extension' => 'docx',
+        ]);
+
+        /** @var Material $material */
+        $material = $this->populator()->populate(
+            $section->class_name,
+            [
+                'name' => 'Name',
+                $section->fields->first()->id => [['id' => $file->id]]
+            ]
+        );
+
+        $this->populator()->flush();
+
+
+        $this->callAuthorizedRouteAction([], ['material' => $material->id, 'section' => $section->id])
+            ->assertOk()
+            ->assertJsonPath('data.' . $section->fields->first()->id . '.0.id', $file->id)
+            ->assertJsonPath('data.' . $section->fields->first()->id . '.0.name', $file->name)
+            ->assertJsonPath('data.' . $section->fields->first()->id . '.0.extension', $file->extension)
+        ;
     }
 }
