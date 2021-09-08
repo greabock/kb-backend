aea97f1d644e8b64e76407db4aeb29d4aa3c3656
Роман Сохарев <greabock@gmail.com>
2021-09-08 16:47:50 +0700

    avatar => photo

--- app/Http/Actions/Api/Auth/Azure/Login/Action.php
+++ app/Http/Actions/Api/Auth/Azure/Login/Action.php
@@ -49,7 +49,7 @@ class Action
             $file = $user->id . '.' . explode('/', $result->getHeader('content-type')[0])[1];
 
             Storage::disk('public')->put('users/' . $file, $result->getBody());
-            $user->photo = 'users/' . $file;
+            $user->photo = url('storage/users' . $file);
 
         } catch (ClientException $e) {
             if ($e->getResponse()->getStatusCode() !== 404) {
