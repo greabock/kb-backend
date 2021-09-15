af7158c26fa5fbd69e0b6394364a3992eb70a0ba
Роман Сохарев <greabock@gmail.com>
2021-09-15 17:58:34 +0700

    FillableName

--- app/Models/User/Group.php
+++ app/Models/User/Group.php
@@ -6,13 +6,18 @@ namespace App\Models\User;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group extends Model
 {
     use SoftDeletes;
 
-    public function users()
+    protected $fillable = [
+        'name'
+    ];
+
+    public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
     }
