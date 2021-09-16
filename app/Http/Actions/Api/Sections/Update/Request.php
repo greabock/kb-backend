48d409a37249d52e72b9b37a1546477ee6559bdd
Роман Сохарев <greabock@gmail.com>
2021-09-16 17:52:12 +0700

    API настройки доступа при сохранении группы

--- app/Http/Actions/Api/Sections/Update/Request.php
+++ app/Http/Actions/Api/Sections/Update/Request.php
@@ -7,6 +7,7 @@ namespace App\Http\Actions\Api\Sections\Update;
 use App\Http\Actions\Api\ApiRequest;
 use App\Validation\Rules\FieldType;
 use Illuminate\Http\Request as BaseRequest;
+use OpenApi\Annotations as OA;
 
 /**
  * @OA\Schema(schema="SectionUpdateRequest",
@@ -15,6 +16,21 @@ use Illuminate\Http\Request as BaseRequest;
  *     @OA\Property(property="is_dictionary", type="boolean"),
  *     @OA\Property(property="is_navigation", type="boolean"),
  *     @OA\Property(property="image", type="string", example="Имя раздела", nullable=true, minimum="1", maximum="255"),
+ *     @OA\Property(property="access", type="string", enum={"all", "only", "except"}),
+ *     @OA\Property(property="config", type="object", nullable=true,
+ *       @OA\Property(property="name", type="string", example="Наименование"),
+ *       @OA\Property(property="description", type="string", example="Что-то тоам про поле 'name'"),
+ *     ),
+ *     @OA\Property(property="users", type="array",
+ *       @OA\Items(type="object", required={"id"},
+ *          @OA\Property(property="id", type="string", example="123e4567-e89b-12d3-a456-426655440000"),
+ *       )
+ *     ),
+ *     @OA\Property(property="groups", type="array",
+ *       @OA\Items(type="object", required={"id"},
+ *          @OA\Property(property="id", type="string", example="123e4567-e89b-12d3-a456-426655440000"),
+ *       )
+ *     ),
  *     @OA\Property(property="fields", type="array",
  *       @OA\Items(type="object",
  *           @OA\Property(property="id", type="string", example="123e4567-e89b-12d3-a456-426655440000"),
@@ -61,7 +77,12 @@ class Request extends ApiRequest
                 'image' => 'nullable|string|max:255',
                 'is_dictionary' => 'sometimes|boolean',
                 'is_navigation' => 'sometimes|boolean',
+                'access' => 'sometimes|nullable|in:all,only,except',
                 'config' => 'nullable',
+                'users' => 'sometimes|array|index_array',
+                'users.*.id' => 'sometimes|uuid|distinct',
+                'groups' => 'sometimes|array|index_array',
+                'groups.*.id' => 'sometimes|uuid|distinct',
                 'fields' => 'sometimes|array|index_array',
                 'fields.*.id' => 'sometimes|uuid|distinct',
                 'fields.*.title' => 'sometimes|string|max:255',
@@ -84,7 +105,10 @@ class Request extends ApiRequest
             'is_dictionary',
             'is_navigation',
             'sort_index',
+            'access',
             'config' => ['name', 'description'],
+            'users' => [['id']],
+            'groups' => [['id']],
             'fields' => [[
                 'id',
                 'title',
