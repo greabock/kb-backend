bb2abeb49ccf0b6f67cbce69c5dd1d602648fe63
Роман Сохарев <sokharev@metasharks.ru>
2021-09-20 15:59:50 +0000

    Fix return value

--- app/Http/Actions/Api/Files/Sign/Action.php
+++ app/Http/Actions/Api/Files/Sign/Action.php
@@ -9,12 +9,12 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class Action
 {
-    public function __invoke(File $file): BinaryFileResponse
+    public function __invoke(File $file)
     {
         return [
             'data' => return URL::temporarySignedRoute(
                 'files.download', now()->addMinutes(5), ['user' => $file->id]
-            );
+            ),
         ];
     }
 }
