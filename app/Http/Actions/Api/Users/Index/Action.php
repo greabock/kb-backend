9173ab539c9fa7a2c96fbf3be9e34d7060ac0eca
Роман Сохарев <greabock@gmail.com>
2021-09-22 19:35:34 +0700

    Исправил условие выборки пользователя

--- app/Http/Actions/Api/Users/Index/Action.php
+++ app/Http/Actions/Api/Users/Index/Action.php
@@ -6,6 +6,7 @@ namespace App\Http\Actions\Api\Users\Index;
 
 use App\Models\User;
 use App\Http\Resources\UserResource;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -15,8 +16,9 @@ class Action
     {
         return UserResource::collection(
             User::where('super', false)
-                ->where('id', '!=', $request->user()->id)
-                ->get()
+                ->orWhere(function (Builder $query) use ($request) {
+                    $query->where('super', true)->where('id', $request->user()->id);
+                })->get()
         );
     }
 }
