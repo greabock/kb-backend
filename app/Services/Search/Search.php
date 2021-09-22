f10553c6137b7ed0d15c9fc8779d650a19c9faac
Роман Сохарев <greabock@gmail.com>
2021-09-22 15:01:03 +0700

    Check isset highlight

--- app/Services/Search/Search.php
+++ app/Services/Search/Search.php
@@ -182,9 +182,9 @@ class Search
             $materials[$i] = [
                 'section' => ['id' => $hit['_index']],
                 'material' => $hit['_source'],
-                'highlight' => collect($hit['highlight'])->mapWithKeys(function ($value, $key) {
+                'highlight' => isset($hit['highlight']) ? collect($hit['highlight'])->mapWithKeys(function ($value, $key) {
                     return [str_replace('_name', '', $key) => $value];
-                }) ?? (object)[],
+                }) : (object)[],
             ];
 
             $materials[$i]['material']['files_count'] = $this->countDocs($fields->fileFields(), $hit['_source']);
