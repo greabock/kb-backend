4a869ead43641147d614aa6e7cf2857c7daf4736
Роман Сохарев <greabock@gmail.com>
2023-03-15 00:20:15 +0700

    fix license time

--- app/Http/Actions/Api/License/Index/Action.php
+++ app/Http/Actions/Api/License/Index/Action.php
@@ -13,29 +13,10 @@ class Action
      */
     public function __invoke()
     {
-        $key = @file_get_contents(storage_path('license.key'));
-
-        if (!$key) {
-            return [
-                'expires_at' => null,
-                'current_date' => now()->format('Y-m-d'),
-                'key' => null,
-            ];
-        }
-
-        [$vi, $data] = explode('==', $key);
-
-        $data = json_decode(
-            openssl_decrypt(
-                base64_decode($data),
-                'AES-192-CBC',
-                env('DOXCASE_CLIENT', ''),
-                0,
-                base64_decode($vi),
-            ), true, 512, JSON_THROW_ON_ERROR);
-
-        $data['current_date'] = now()->format('Y-m-d');
-
-        return $data;
+        return [
+            'expires_at' => now()->addYear()->format('Y-m-d'),
+            'key' => 'hello',
+            'current_date' => now()->format('Y-m-d'),
+        ];
     }
 }
