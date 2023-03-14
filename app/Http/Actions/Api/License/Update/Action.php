f36e800d92f98e39d7417662cdc167f4dff76c5e
Роман Сохарев <greabock@gmail.com>
2023-03-14 23:55:11 +0700

    Fix license

--- app/Http/Actions/Api/License/Update/Action.php
+++ app/Http/Actions/Api/License/Update/Action.php
@@ -15,24 +15,6 @@ class Action
      */
     public function __invoke(Request $request)
     {
-        [$vi, $data] = explode('==', $key = $request->get('key'));
-        $client = env('DOXCASE_CLIENT');
-
-        $data = json_decode(
-            openssl_decrypt(
-                base64_decode($data),
-                'AES-192-CBC',
-                $client,
-                0,
-                base64_decode($vi),
-            ), true, 512, JSON_THROW_ON_ERROR);
-
-        if ($data['key'] !== $client) {
-            throw ValidationException::withMessages(['key' => ['Invalid key']]);
-        }
-
-        file_put_contents(storage_path('license.key'), $key);
-
         return response('', 200);
     }
 }
